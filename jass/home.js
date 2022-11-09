aa2dc484259fcd8b811b73b785537362adee89da
jainilsoni1706 <jainilsoni1706@gmail.com>
2022-11-09 23:17:24 +0530

    jass

---
+++ jass/home.js
@@ -0,0 +1,12 @@
+fout= () => {
+
+    var myName = document.querySelector('.menuSearch');
+    if(myName.value=="jainil" || myName.value=="Ranpara" || myName.value=="Jainil" || myName.value=="Jainil" || myName.value=="JainiL"){
+        myName.style.color = "#57d658";
+    }
+    else{
+        myName.style.color = "whitesmoke";
+    }
+}
+
+
